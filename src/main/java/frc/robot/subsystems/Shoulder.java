1725d1288f80bc045e21ff17952c86348326eebe
Rodney Lewis <rodneylewis95@gmail.com>
2024-01-26 20:21:40 -0500

    Update Shoulder.java
    - Implement shoulder spec

--- src/main/java/frc/robot/subsystems/Shoulder.java
+++ src/main/java/frc/robot/subsystems/Shoulder.java
@@ -10,13 +10,14 @@ import com.revrobotics.SparkMaxAlternateEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkBase.SoftLimitDirection;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shoulder extends SubsystemBase {
 
-  public enum ShoulderRotation {
+  public enum Position {
     LIMIT(100),
     AMP(80),
     PREP_CLIMB(60),
@@ -24,63 +25,79 @@ public class Shoulder extends SubsystemBase {
     SPEAKER(10),
     HOME(0);
 
-    public final double positionInTicks;
+    // Default value is rotations
+    public final float value;
 
-    ShoulderRotation(double positionInTicks) {
-      this.positionInTicks = positionInTicks;
+    Position(float value) {
+      this.value = value;
     }
   }
 
-  private final int LEFT_CAN_ID = 11;
-  private final int RIGHT_CAN_ID = 12;
-  private final CANSparkMax left;
-  private final CANSparkMax right;
+  private final int LEFT_ROTATOR_CAN_ID = 11;
+  private final int RIGHT_ROTATOR_CAN_ID = 12;
+  private final int ALLOWABLE_CLOSED_LOOP_ERROR = 8192;
+  private final int ENCODER_OFFSET = 0;
+
+  private final CANSparkMax leftRotator;
+  private final CANSparkMax rightRotator;
   private final SparkPIDController sparkPIDController;
   private final RelativeEncoder throughBoreEncoder;
 
   public Shoulder() {
-    right = new CANSparkMax(RIGHT_CAN_ID, MotorType.kBrushless);
-    right.restoreFactoryDefaults();
-    right.setSmartCurrentLimit(60);
-    right.setIdleMode(IdleMode.kBrake);
-    right.setClosedLoopRampRate(0.5);
-    right.setOpenLoopRampRate(0.5);
+    rightRotator = new CANSparkMax(RIGHT_ROTATOR_CAN_ID, MotorType.kBrushless);
+    rightRotator.restoreFactoryDefaults();
+    rightRotator.setSmartCurrentLimit(60);
+    rightRotator.setIdleMode(IdleMode.kBrake);
+    rightRotator.setClosedLoopRampRate(0.5);
+    rightRotator.setOpenLoopRampRate(0.5);
+    rightRotator.setSoftLimit(SoftLimitDirection.kForward, Position.LIMIT.value);
+    rightRotator.setSoftLimit(SoftLimitDirection.kReverse, Position.HOME.value);
+    rightRotator.enableSoftLimit(SoftLimitDirection.kForward, true);
+    rightRotator.enableSoftLimit(SoftLimitDirection.kReverse, true);
+    rightRotator.setInverted(false);
 
-    left = new CANSparkMax(LEFT_CAN_ID, MotorType.kBrushless);
-    left.restoreFactoryDefaults();
-    left.setSmartCurrentLimit(60);
-    left.setIdleMode(IdleMode.kBrake);
-    left.setClosedLoopRampRate(0.5);
-    left.setOpenLoopRampRate(0.5);
-    left.follow(right, true);
+    leftRotator = new CANSparkMax(LEFT_ROTATOR_CAN_ID, MotorType.kBrushless);
+    leftRotator.restoreFactoryDefaults();
+    leftRotator.setSmartCurrentLimit(60);
+    leftRotator.setIdleMode(IdleMode.kBrake);
+    leftRotator.setClosedLoopRampRate(0.5);
+    leftRotator.setOpenLoopRampRate(0.5);
+    leftRotator.follow(rightRotator, true);
 
-    throughBoreEncoder = right.getAlternateEncoder(SparkMaxAlternateEncoder.Type.kQuadrature, 8192);
-    sparkPIDController = right.getPIDController();
+    throughBoreEncoder = rightRotator.getAlternateEncoder(SparkMaxAlternateEncoder.Type.kQuadrature, 8192);
+    throughBoreEncoder.setInverted(false);
+
+    sparkPIDController = rightRotator.getPIDController();
     sparkPIDController.setFeedbackDevice(throughBoreEncoder);
     sparkPIDController.setP(0.1);
     sparkPIDController.setI(0);
     sparkPIDController.setD(1);
+    sparkPIDController.setFF(0);
     sparkPIDController.setOutputRange(-1, 1);
   }
 
-  public void setPosition(double rotations) {
-    sparkPIDController.setReference(rotations, ControlType.kPosition);
+  public void setPosition(double targetPosition) {
+    sparkPIDController.setReference(targetPosition, ControlType.kPosition);
   }
 
   public void setDutyCycle(double percentOut) {
-    right.set(percentOut);
+    rightRotator.set(percentOut);
   }
 
   public void stop() {
-    right.set(0);
+    rightRotator.set(0);
   }
 
   public double getPosition() {
-    return throughBoreEncoder.getPosition();
+    return throughBoreEncoder.getPosition() + ENCODER_OFFSET;
   }
 
   public void resetPosition() {
     throughBoreEncoder.setPosition(0);
+  }
+
+  public boolean isAtSetPoint(double targetPosition) {
+    return Math.abs(targetPosition - getPosition()) < ALLOWABLE_CLOSED_LOOP_ERROR;
   }
 
   @Override
