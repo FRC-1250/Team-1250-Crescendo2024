4ea7260954b65725c0980c3ea45adc6cd5593329
Rodney Lewis <rodneylewis95@gmail.com>
2024-01-29 17:45:17 -0500

    Update SetShoulderDutyCycle.java
    - Stop motors when command ends

--- src/main/java/frc/robot/commands/SetShoulderDutyCycle.java
+++ src/main/java/frc/robot/commands/SetShoulderDutyCycle.java
@@ -29,7 +29,9 @@ public class SetShoulderDutyCycle extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    shoulder.setDutyCycle(0);
+  }
 
   // Returns true when the command should end.
   @Override
