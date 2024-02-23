acf9b17c9a89cd310731c63dfad400c2ccfc1a67
1250-programming <114453660+1250-programming@users.noreply.github.com>
2024-02-22 21:00:16 -0500

    Adding to the lgihts

--- src/main/java/frc/robot/subsystems/SystemLights.java
+++ src/main/java/frc/robot/subsystems/SystemLights.java
@@ -5,24 +5,47 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdle.VBatOutputMode;
 import com.ctre.phoenix.led.CANdleConfiguration;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Shoulder.Position;
 
 public class SystemLights extends SubsystemBase {
   /** Creates a new SystemLights. */
+  private final indexer indexer;
+  private final Shoulder shoulder;
   private final CANdle candle = new CANdle(0); 
-    private final int LedCount = 300;
-  public SystemLights(CANdle candle) {
+  private final int LedCount = 300;
+
+
+  public double getVbat() { return candle.getBusVoltage(); }
+  public double get5V() { return candle.get5VRailVoltage(); }
+  public double getCurrent() { return candle.getCurrent(); }
+  public double getTemperature() { return candle.getTemperature(); }
+  public void configBrightness(double percent) { candle.configBrightnessScalar(percent, 0); }
+  public void configLos(boolean disableWhenLos) { candle.configLOSBehavior(disableWhenLos, 0); }
+  public void configLedType(LEDStripType type) { candle.configLEDType(type, 0); }
+  public void configStatusLedBehavior(boolean offWhenActive) { candle.configStatusLedState(offWhenActive, 0); }
+
+
+  public SystemLights(CANdle candle, indexer indexer, Shoulder shoulder) {
     CANdleConfiguration configAll = new CANdleConfiguration();
     configAll.disableWhenLOS = false;
     configAll.brightnessScalar = 0.1;
     configAll.vBatOutputMode = VBatOutputMode.Modulated;
     candle.configAllSettings(configAll, 100);
+    this.indexer = indexer;
+    this.shoulder = shoulder;
+
+    
   }
 
-  public void setLEDs(int r, int g, int b) {
+   
+  
+    public void setLEDs(int r, int g, int b) {
     candle.setLEDs(r, g, b);
   }
 
@@ -32,5 +55,13 @@ public class SystemLights extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if(indexer.iscentered() == true) {
+      setLEDs(LedCount, LedCount, LedCount);
+    }
+    if(MathUtil.isNear(Position.HOME.value, shoulder.getPosition(), 0.005, 0, 1)) {
+      setLEDs(LedCount, LedCount, LedCount);
+    } else {
+      setLEDs(LedCount, LedCount, LedCount);
+    }
   }
 }
