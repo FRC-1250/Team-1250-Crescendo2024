d3790914c7174d5de6f423577b7a5f41b2690e9a
Rodney Lewis <rodneylewis95@gmail.com>
2024-03-28 18:47:08 -0400

    Intake + launcher cleanup

--- src/main/java/frc/robot/subsystems/launcher.java
+++ src/main/java/frc/robot/subsystems/launcher.java
@@ -4,80 +4,69 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkPIDController;
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class launcher extends SubsystemBase {
-  private int LSHOOTER = 20;
-  private int RSHOOTER = 21;
-  public final int maxRPM = 6380;
+  private final int LEFT_LAUNCHER_CAN_ID = 20;
+  private final int RIGHT_LAUNCHER_CAN_ID = 21;
+  public final int FALCON_500_MAX_RPM = 6380;
   public final int PODIUM_TARGET_RPM_LEFT = 5000;
   public final int PODIUM_TARGET_RPM_RIGHT = 5000;
   public final int SPEAKER_TARGET_RPM_LEFT = 4500;
   public final int SPEAKER_TARGET_RPM_RIGHT = 4500;
   public final int AMP_TARGET_RPM_LEFT = 1500;
-  public final int AMP_TARGET_RPM_RIGHT = 1500; 
-  private final VelocityVoltage velocity = new VelocityVoltage(0, 0, true, 0, 0, false, false, false);
-private final TalonFX rightLauncher;
-private final TalonFX leftLauncher;
+  public final int AMP_TARGET_RPM_RIGHT = 1500;
 
-
+  private final VelocityVoltage leftVelocityControl;
+  private final VelocityVoltage rightVelocityControl;
+  private final TalonFX rightLauncher;
+  private final TalonFX leftLauncher;
 
   /** Creates a new shooter. */
 
-  
   public launcher() {
     TalonFXConfiguration configs = new TalonFXConfiguration();
-    configs.Slot0.kP = .11;
+    configs.Slot0.kP = 0.11;
     configs.Slot0.kI = 0;
     configs.Slot0.kD = 0.0001;
-    configs.Slot0.kV = .12;
-    configs.CurrentLimits.StatorCurrentLimit = 45;
-    configs.CurrentLimits.StatorCurrentLimitEnable = true;
-    configs.CurrentLimits.SupplyCurrentLimit = 45;
+    configs.Slot0.kV = 0.12;
+    configs.CurrentLimits.SupplyCurrentLimit = 50;
     configs.CurrentLimits.SupplyCurrentLimitEnable = true;
+    configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-    rightLauncher = new TalonFX(RSHOOTER, "rio");
-    rightLauncher.getConfigurator().apply(new TalonFXConfiguration());
+    rightLauncher = new TalonFX(RIGHT_LAUNCHER_CAN_ID, "rio");
     rightLauncher.getConfigurator().apply(configs);
+    rightVelocityControl = new VelocityVoltage(0, 0, false, 0, 0, false, false, false);
 
-
-    leftLauncher = new TalonFX(LSHOOTER, "rio");  
-    leftLauncher.getConfigurator().apply(new TalonFXConfiguration());
+    leftLauncher = new TalonFX(LEFT_LAUNCHER_CAN_ID, "rio");
     leftLauncher.getConfigurator().apply(configs);
-
+    leftVelocityControl = new VelocityVoltage(0, 0, false, 0, 0, false, false, false);
   }
 
-public void SetDutyOutlaunch(double percent) {
-  rightLauncher.set(percent);
-  leftLauncher.set(percent);
-}
+  public void SetDutyOutlaunch(double percent) {
+    rightLauncher.set(percent);
+    leftLauncher.set(percent);
+  }
 
-public double getRightLauncherRPM() {
- return rightLauncher.getVelocity().getValue();
-}
+  public double getRightLauncherRPM() {
+    return rightLauncher.getVelocity().getValue();
+  }
 
-public double getLeftLauncherRPM() {
- return leftLauncher.getVelocity().getValue();
-}
+  public double getLeftLauncherRPM() {
+    return leftLauncher.getVelocity().getValue();
+  }
 
+  public void SetLauncherVelocity(double right, double left) {
+    rightLauncher.setControl(rightVelocityControl.withVelocity(right / 60));
+    leftLauncher.setControl(leftVelocityControl.withVelocity(left / 60));
+  }
 
-public void SetLauncherVelocity(double right, double left) {
-    rightLauncher.setControl(velocity.withVelocity(right/60));
-    leftLauncher.setControl(velocity.withVelocity(left/60));
-}
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Launcher/Right RPM", getRightLauncherRPM());
