9df5d129acd0b16257a023ac575336c32b50c883
1250-tars <1250robotics+programming@gmail.com>
2024-05-07 16:45:30 -0400

    op changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.SetShoulderDutyCycle;
 import frc.robot.commands.SetShoulderPosition;
@@ -66,6 +67,7 @@ public class RobotContainer {
       .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
   private final CommandXboxController drivXboxController = new CommandXboxController(0);
+  private final CommandPS4Controller operatorPS4Controller = new CommandPS4Controller(1);
 
   public RobotContainer() {
     SmartDashboard.putData("Intake/Command", intake);
@@ -93,16 +95,29 @@ public class RobotContainer {
             .withRotationalRate(-drivXboxController.getRightX() * TunerConstants.MaxAngularRate * 0.75),
             "Robot centric drive"));
 
-    drivXboxController.rightBumper().onTrue(new IntakeCenterNote(intake, shoulder, indexer, 1.0));
-    drivXboxController.rightTrigger().whileTrue(new FireNote(indexer, launcher, shoulder));
-    drivXboxController.a().whileTrue(new SetIntakeDutyCycle(intake, -1));
-    drivXboxController.a().whileTrue(new SetIndexDutyCycle(indexer, -.1));
+
+    operatorPS4Controller.R1().onTrue(new IntakeCenterNote(intake, shoulder, indexer, 1.0));
+    //drivXboxController.rightBumper().onTrue(new IntakeCenterNote(intake, shoulder, indexer, 1.0));
+    operatorPS4Controller.R2().whileTrue(new FireNote(indexer, launcher, shoulder));
+    //drivXboxController.rightTrigger().whileTrue(new FireNote(indexer, launcher, shoulder));
+    operatorPS4Controller.cross().whileTrue(new SetIntakeDutyCycle(intake, -1));
+    //drivXboxController.a().whileTrue(new SetIntakeDutyCycle(intake, -1));
+    operatorPS4Controller.cross().whileTrue(new SetIndexDutyCycle(indexer, 0.1));
+    //drivXboxController.a().whileTrue(new SetIndexDutyCycle(indexer, -.1));
+    operatorPS4Controller.cross().whileTrue(new SetLauncherDutyCycle(launcher, -0.5));
     drivXboxController.a().whileTrue(new SetLauncherDutyCycle(launcher, -.5));
-    drivXboxController.b().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.SPEAKER_PODIUM));
-    drivXboxController.leftTrigger().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.SPEAKER));
-    drivXboxController.leftBumper().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.AMP));
-    drivXboxController.pov(0).whileTrue(new SetShoulderDutyCycle(shoulder, 0.5));
-    drivXboxController.pov(180).whileTrue(new SetShoulderDutyCycle(shoulder, -0.5));
+    //drivXboxController.b().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.SPEAKER_PODIUM));
+    operatorPS4Controller.L2().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.SPEAKER));
+    //drivXboxController.leftTrigger().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.SPEAKER));
+    operatorPS4Controller.L1().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.AMP));
+    //drivXboxController.leftBumper().onTrue(new SetPositionAndShooterSpeed(shoulder, launcher, Position.AMP));
+    operatorPS4Controller.pov(0).whileTrue(new SetShoulderDutyCycle(shoulder, 0.5));
+    //drivXboxController.pov(0).whileTrue(new SetShoulderDutyCycle(shoulder, 0.5));
+    operatorPS4Controller.pov(180).whileTrue(new SetShoulderDutyCycle(shoulder, -0.5));
+    //drivXboxController.pov(180).whileTrue(new SetShoulderDutyCycle(shoulder, -0.5));
+    operatorPS4Controller.triangle().onTrue(new SetIntakeDutyCycle(intake, 0));
+    operatorPS4Controller.triangle().onTrue(new SetLauncherDutyCycle(launcher, 0));
+    operatorPS4Controller.triangle().onTrue(new SetShoulderPosition(shoulder, Position.HOME.value));
     drivXboxController.y().onTrue(new SetIntakeDutyCycle(intake, 0));
     drivXboxController.y().onTrue(new SetLauncherDutyCycle(launcher, 0));
     drivXboxController.y().onTrue(new SetShoulderPosition(shoulder, Position.HOME.value));    
