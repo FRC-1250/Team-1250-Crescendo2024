6690757fedce00967a378fe1dc5d1ccb16fe69a7
Rodney Lewis <rodneylewis95@gmail.com>
2024-05-31 08:44:46 -0400

    Update Shoulder.java
    - General cleanup and formatting
    - Switched PositionVoltage to PositionDutyCycle, this is like using position control in v5 see https://pro.docs.ctr-electronics.com/en/stable/docs/migration/migration-guide/control-requests-guide.html#control-types

--- src/main/java/frc/robot/subsystems/Shoulder.java
+++ src/main/java/frc/robot/subsystems/Shoulder.java
@@ -4,20 +4,19 @@
 
 package frc.robot.subsystems;
 
-import org.opencv.core.Mat;
+import java.util.function.Supplier;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
-import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
-import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.ReverseLimitSourceValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 import edu.wpi.first.math.MathUtil;
@@ -46,38 +45,41 @@ public class Shoulder extends SubsystemBase {
   private final int CANCODER_CAN_ID = 32;
   private final double CLOSED_LOOP_TOLERANCE = 0.003; // Closed loop tolerance in degrees
   private final double ENCODER_OFFSET = 0.0233; // Offset value to normalize the encoder position to 0 when at home
+  private final NeutralOut BRAKE = new NeutralOut();
   private final TalonFX leftRotator;
   private final TalonFX rightRotator;
   private final CANcoder cancoder;
-  private final PositionVoltage vPostion = new PositionVoltage(0, 0, true, 0, 0, false, false, false);
+  private final DutyCycleOut dutyCycleOut;
+  private final PositionDutyCycle positionDutyCycle;
+  private final Supplier<Double> positionSupplier;
 
   public Shoulder() {
-
     CANcoderConfiguration configuration = new CANcoderConfiguration();
     configuration.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
     configuration.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
     configuration.MagnetSensor.MagnetOffset = ENCODER_OFFSET;
     cancoder = new CANcoder(CANCODER_CAN_ID, "rio");
     cancoder.getConfigurator().apply(configuration);
-    cancoder.getPosition().setUpdateFrequency(100);
+    positionSupplier = cancoder.getPosition().asSupplier();
 
     TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
+
+    // Configure the CANCoder as our feedback device
     talonFXConfiguration.Feedback.FeedbackRemoteSensorID = cancoder.getDeviceID();
     talonFXConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+    talonFXConfiguration.Feedback.SensorToMechanismRatio = 1;
 
     // PID for the Shoulder
     talonFXConfiguration.Slot0.kP = 10;
     talonFXConfiguration.Slot0.kI = 0;
     talonFXConfiguration.Slot0.kD = 0;
 
-    // How much power is being supplied to the motors
+    // Maxmimum amps supplied to the motors
     talonFXConfiguration.CurrentLimits.SupplyCurrentLimit = 25;
     talonFXConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
 
     // The mode for which the motor is set to when not recieving a command
     talonFXConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-
-    talonFXConfiguration.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 0.25;
 
     rightRotator = new TalonFX(RIGHT_ROTATOR_CAN_ID, "rio");
     rightRotator.getConfigurator().apply(talonFXConfiguration);
@@ -85,33 +87,39 @@ public class Shoulder extends SubsystemBase {
     leftRotator = new TalonFX(LEFT_ROTATOR_CAN_ID, "rio");
     leftRotator.getConfigurator().apply(talonFXConfiguration);
     leftRotator.setControl(new Follower(rightRotator.getDeviceID(), true));
+
+    dutyCycleOut = new DutyCycleOut(0);
+    positionDutyCycle = new PositionDutyCycle(0);
   }
 
-  public boolean isforwardlimit() {
+  public boolean isForwardLimit() {
     return MathUtil.isNear(Position.AMP.value, getPosition(), CLOSED_LOOP_TOLERANCE);
   }
 
-  public boolean isreverselimt() {
+  public boolean isReverseLimit() {
     return MathUtil.isNear(Position.HOME.value, getPosition(), CLOSED_LOOP_TOLERANCE);
   }
 
   public void setPosition(double targetPosition) {
-    rightRotator.setControl(vPostion.withPosition(targetPosition)
-    .withFeedForward(3)
-    .withLimitForwardMotion(isforwardlimit())
-    .withLimitReverseMotion(isreverselimt()));
+    rightRotator.setControl(positionDutyCycle.withPosition(targetPosition)
+        .withFeedForward(0.25)
+        .withSlot(0)
+        .withLimitForwardMotion(isForwardLimit())
+        .withLimitReverseMotion(isReverseLimit()));
   }
 
   public void setDutyCycle(double percentOut) {
-    rightRotator.set(percentOut);
+    rightRotator.setControl(dutyCycleOut.withOutput(percentOut)
+        .withLimitForwardMotion(isForwardLimit())
+        .withLimitReverseMotion(isReverseLimit()));
   }
 
   public void stop() {
-    rightRotator.set(0);
+    rightRotator.setControl(BRAKE);
   }
 
   public double getPosition() {
-    return cancoder.getPosition().getValueAsDouble();
+    return positionSupplier.get();
   }
 
   public boolean isAtSetPoint(double targetPosition) {
