350428befecf3062466fe252a757230c0c4af587
Rodney Lewis <rodneylewis95@gmail.com>
2024-06-30 11:00:01 -0400

    Update CommandSwerveDrivetrain.java

--- src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
@@ -3,9 +3,6 @@ package frc.robot.subsystems.drive;
 import java.util.function.Supplier;
 
 import com.ctre.phoenix6.Utils;
-import com.ctre.phoenix6.configs.ClosedLoopRampsConfigs;
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
